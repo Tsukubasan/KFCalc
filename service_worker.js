58d835a2d74a021df726e3b601a7b1d199e7c6a8
Tsukubasan <Tsukubasan@users.noreply.github.com>
2019-05-07 00:19:45 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,3 @@
-
 const CACHE_VERSION = '201905070002';
 const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
 
@@ -21,55 +20,55 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return cacheNames.filter((cacheName) => {
-        // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
-        return cacheName.startsWith(`${registration.scope}!`) &&
-               cacheName !== CACHE_NAME;
-      });
-    }).then((cachesToDelete) => {
-      return Promise.all(cachesToDelete.map((cacheName) => {
-        // いらないキャッシュを削除する
-        return caches.delete(cacheName);
-      }));
-    })
-  );
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return cacheNames.filter((cacheName) => {
+                // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
+                return cacheName.startsWith(`${registration.scope}!`) &&
+                    cacheName !== CACHE_NAME;
+            });
+        }).then((cachesToDelete) => {
+            return Promise.all(cachesToDelete.map((cacheName) => {
+                // いらないキャッシュを削除する
+                return caches.delete(cacheName);
+            }));
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-    .then((response) => {
-      // キャッシュ内に該当レスポンスがあれば、それを返す
-      if (response) {
-        return response;
-      }
-
-      // 重要：リクエストを clone する。リクエストは Stream なので
-      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-      // 必要なので、リクエストは clone しないといけない
-      let fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest)
+    event.respondWith(
+        caches.match(event.request)
         .then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            // キャッシュする必要のないタイプのレスポンスならそのまま返す
-            return response;
-          }
+            // キャッシュ内に該当レスポンスがあれば、それを返す
+            if (response) {
+                return response;
+            }
 
-          // 重要：レスポンスを clone する。レスポンスは Stream で
-          // ブラウザ用とキャッシュ用の2回必要。なので clone して
-          // 2つの Stream があるようにする
-          let responseToCache = response.clone();
+            // 重要：リクエストを clone する。リクエストは Stream なので
+            // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+            // 必要なので、リクエストは clone しないといけない
+            let fetchRequest = event.request.clone();
 
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
+            return fetch(fetchRequest)
+                .then((response) => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        // キャッシュする必要のないタイプのレスポンスならそのまま返す
+                        return response;
+                    }
 
-          return response;
-        });
-    })
-  );
+                    // 重要：レスポンスを clone する。レスポンスは Stream で
+                    // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                    // 2つの Stream があるようにする
+                    let responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then((cache) => {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                });
+        })
+    );
 });
